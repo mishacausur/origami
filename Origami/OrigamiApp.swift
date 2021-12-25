07f869ce1cb28c58fe04d7f7040331ff3a7518fb
Misha Causur <mishacausur@mail.ru>
2021-12-25 12:48:02 +0500

    Initial Commit

---
+++ Origami/OrigamiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrigamiApp.swift
+//  Origami
+//
+//  Created by Misha Causur on 25.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct OrigamiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
