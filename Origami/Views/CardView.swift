1cbd82e21e8e91b0aa8b256de06b9add62dbf06b
Misha Causur <mishacausur@mail.ru>
2021-12-25 16:23:44 +0500

    done task 2

--- Origami/Views/CardView.swift
+++ Origami/Views/CardView.swift
@@ -14,35 +14,104 @@ struct CardView: View {
         ZStack {
             if !show {
                 VStack {
-                    Text("Иванов Иван Иванович")
-                        .font(.headline)
-                        .matchedGeometryEffect(id: "subject", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("66:41:0205009:1393 ")
-                        .matchedGeometryEffect(id: "object", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                    Spacer()
+                    VStack(alignment: .leading, spacing: 12) {
+                        Text("Иванов Иван Иванович")
+                            .font(.headline)
+                            .matchedGeometryEffect(id: "subject", in: namespace)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("66:41:0205009:1393 ")
+                            .matchedGeometryEffect(id: "object", in: namespace)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("Рисков нет")
+                            .font(.headline.weight(.semibold))
+                            .matchedGeometryEffect(id: "riscs", in: namespace)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .padding(20)
+                    .background(
+                        Rectangle()
+                            .fill(.ultraThinMaterial)
+                            .mask {
+                                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            }
+                            .blur(radius: 30)
+                            .matchedGeometryEffect(id: "blur", in: namespace)
+                    )
+                    
                 }
                 .foregroundStyle(.white)
                 .background(
-                    Color.blue.matchedGeometryEffect(id: "color", in: namespace))
-            } else {
-                VStack {
-                    Spacer()
-                    Text("Иванов Иван Иванович")
-                        .font(.headline)
-                        .matchedGeometryEffect(id: "subject", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("66:41:0205009:1393 ")
-                        .matchedGeometryEffect(id: "object", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                    Image("back")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "color", in: namespace))
+                .mask {
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace)
                 }
-                .foregroundStyle(.black)
-                .background(
-                    Color.gray.matchedGeometryEffect(id: "color", in: namespace))
+                .frame(height: 300)
+                .padding(20)
+            } else {
+                ScrollView {
+                    VStack {
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 500)
+                    .background(
+                        Image("back")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .matchedGeometryEffect(id: "color", in: namespace))
+                    .mask {
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .matchedGeometryEffect(id: "mask", in: namespace)
+                    }
+                    .overlay(
+                        VStack(alignment: .leading, spacing: 12) {
+                            Text("Дата проверки: 20 декабря 2021 года")
+                                .font(.footnote)
+                            Text("Иванов Иван Иванович")
+                                .font(.headline)
+                                .matchedGeometryEffect(id: "subject", in: namespace)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text("66:41:0205009:1393 ")
+                                .matchedGeometryEffect(id: "object", in: namespace)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Divider()
+                            HStack {
+                                Image("ok")
+                                    .resizable()
+                                    .frame(width: 26, height: 25)
+                                    .padding(4)
+                                    .background(.ultraThinMaterial, in:
+                                                    RoundedRectangle(cornerRadius: 10, style: .continuous))
+                                Text("Рисков нет")
+                                    .font(.headline.weight(.semibold))
+                                    .matchedGeometryEffect(id: "riscs", in: namespace)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    
+                            }
+                        }
+                            .padding(20)
+                            .background(
+                                Rectangle()
+                                    .fill(.ultraThinMaterial)
+                                    .mask {
+                                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                                    }
+                                    .matchedGeometryEffect(id: "blur", in: namespace)
+                            )
+                            .offset(y: 250)
+                            .padding(20)
+                    )
+                }
+                .ignoresSafeArea()
             }
         }
         .onTapGesture {
-            withAnimation {
+            withAnimation(.spring(response: 0.6, dampingFraction: 0.7)) {
                 show.toggle()
             }
         }
