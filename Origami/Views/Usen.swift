38369d7e18bcfc9791d8df0447656b4c3e8381f6
Misha Causur <mishacausur@mail.ru>
2024-11-15 22:47:52 +0500

    more fix

--- Origami/Views/Usen.swift
+++ Origami/Views/Usen.swift
@@ -74,27 +74,3 @@ struct AllView: View {
         .background(.clear)
     }
 }
-
-struct AdaptiveSheet<T: View>: ViewModifier {
-    let sheetContent: T
-    @Binding var isPresented: Bool
-    let detents : [UISheetPresentationController.Detent]
-    let smallestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier?
-    let prefersScrollingExpandsWhenScrolledToEdge: Bool
-    let prefersEdgeAttachedInCompactHeight: Bool
-    
-    init(isPresented: Binding<Bool>, detents : [UISheetPresentationController.Detent] = [.medium(), .large()], smallestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier? = .medium, prefersScrollingExpandsWhenScrolledToEdge: Bool = false, prefersEdgeAttachedInCompactHeight: Bool = true, @ViewBuilder content: @escaping () -> T) {
-        self.sheetContent = content()
-        self.detents = detents
-        self.smallestUndimmedDetentIdentifier = smallestUndimmedDetentIdentifier
-        self.prefersEdgeAttachedInCompactHeight = prefersEdgeAttachedInCompactHeight
-        self.prefersScrollingExpandsWhenScrolledToEdge = prefersScrollingExpandsWhenScrolledToEdge
-        self._isPresented = isPresented
-    }
-    func body(content: Content) -> some View {
-        ZStack{
-            content
-            CustomSheet_UI(isPresented: $isPresented, detents: detents, smallestUndimmedDetentIdentifier: smallestUndimmedDetentIdentifier, prefersScrollingExpandsWhenScrolledToEdge: prefersScrollingExpandsWhenScrolledToEdge, prefersEdgeAttachedInCompactHeight: prefersEdgeAttachedInCompactHeight, content: {sheetContent}).frame(width: 0, height: 0)
-        }
-    }
-}
